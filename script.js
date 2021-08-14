23eac5e87321b9d1a9df5906c22420dc4c1c9af2
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-08-14 08:26:20 +0530

    1.0

---
+++ script.js
@@ -0,0 +1,29 @@
+var btn=document.querySelector('#check')
+var x_btn=document.querySelector(".clr")
+
+var note=document.querySelector(".notice")
+
+var inp=document.querySelector('#date-of-birth')
+var luck=document.querySelector('#lucky-number')
+var outputTxt=document.querySelector('#output')
+
+
+btn.addEventListener('click',function clickHandler(){
+	const inputval=((inp.value).split('-')).join("");
+	const luckval=luck.value;
+	console.log(inputval)
+	let sum=0;
+	for(letter of inputval){
+		sum=sum+Number(letter);
+	}
+	if(sum%luckval==0){
+		outputTxt.innerHTML=`you are lucky`
+	}
+	else{
+		outputTxt.innerHTML="not lucky"
+	}
+});
+
+x_btn.addEventListener("click",function clearNotice(){
+	note.style.display="none"
+})
